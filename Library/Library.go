25fe79642cc915609b9dd8ac9e10d0c914d9a0cd
D0anial <230412@astanait.edu.kz>
2024-12-21 13:26:37 +0500

    Initial commit

---
+++ Library/Library.go
@@ -0,0 +1,65 @@
+package Library
+
+import (
+	"fmt"
+)
+
+type Book struct {
+	ID         string
+	Name       string
+	Author     string
+	isBorrowed bool
+}
+
+type Library struct {
+	Books map[string]Book
+}
+
+func NewLibrary() *Library {
+	return &Library{make(map[string]Book)}
+}
+func (l *Library) AddBook(book Book) {
+	if _, exists := l.Books[book.ID]; exists {
+		fmt.Println("Book already exists")
+		return
+	}
+
+	l.Books[book.ID] = book
+	fmt.Println("Book added")
+}
+
+func (l *Library) BorrowBook(id string) {
+	book, exists := l.Books[id]
+	if !exists {
+		fmt.Println("There is no book with this id")
+		return
+	}
+
+	if book.isBorrowed {
+		fmt.Println("Book is already borrowed")
+		return
+	}
+	book.isBorrowed = true
+	fmt.Println("Book successfully borrowed")
+}
+
+func (l *Library) ReturnBook(id string) {
+	book, exists := l.Books[id]
+	if !exists {
+		fmt.Println("There is no book with this id")
+		return
+	}
+
+	if !book.isBorrowed {
+		fmt.Println("Book is already returned")
+		return
+	}
+	book.isBorrowed = false
+	fmt.Println("Book returned borrowed")
+}
+
+func (l *Library) ListBooks() {
+	for _, book := range l.Books {
+		fmt.Printf("BOok #{%s}: {%d} by {%d}", book.ID, book.Name, book.Author)
+	}
+}
---
+++ Library/go.mod
@@ -0,0 +1,3 @@
+module github.com/BolatDanial/Assignment1/Library
+
+go 1.23.2
